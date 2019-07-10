4381b9ad478533f0540de559bb8e5a67cff37685
rpearso2 <hopetagfaithin@gmail.com>
2019-07-10 15:33:22 -0400

    Added Products Controller

---
+++ GiveNTake/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiveNTake.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Products")]
+    public class ProductsController : Controller
+    {
+        public string[] GetProducts()
+        {
+            return new[]
+            {
+                "1 - Microwave",
+                "2 - Washing Machine",
+                "3 - Mirror"
+            };
+        }
+    }
+}
